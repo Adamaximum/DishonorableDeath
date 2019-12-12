96226f211bb37ce7b94bade91a7a87721e31a03a
Adam Goren <amg1090@nyu.edu>
2019-12-12 02:31:18 -0500

    Added several sounds and a new font, retyped some text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,7 +38,10 @@ public class PlayerController : MonoBehaviour
 
     public bool hellContact;
 
-    public Color[] transparentLevel; 
+    public Color[] transparentLevel;
+
+    AudioSource source;
+    public AudioClip[] soundFX;
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +49,8 @@ public class PlayerController : MonoBehaviour
         playerRB = GetComponent<Rigidbody2D>();
 
         playerSR = GetComponent<SpriteRenderer>();
+
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -152,6 +157,7 @@ public class PlayerController : MonoBehaviour
             if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && jumpCheck == true)
             {
                 playerRB.velocity += Vector2.up * jumpVelocity;
+                source.PlayOneShot(soundFX[3]);
             }
         }
         else if (playerState == 1) // Jumping is Infinite
@@ -159,6 +165,7 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
                 playerRB.velocity = Vector2.up * jumpVelocity;
+                source.PlayOneShot(soundFX[3]);
             }
         }
         if (playerState < 2) // Fall Multiplier is not active when Hell is active
@@ -171,9 +178,9 @@ public class PlayerController : MonoBehaviour
     {
         float rayCastDist = 1;
         RaycastHit2D hitLeft;
-        Vector2 rayPositionLeft = new Vector2(transform.position.x - 0.4f, transform.position.y);
+        Vector2 rayPositionLeft = new Vector2(transform.position.x - 0.3f, transform.position.y);
         RaycastHit2D hitRight;
-        Vector2 rayPositionRight = new Vector2(transform.position.x + 0.4f, transform.position.y);
+        Vector2 rayPositionRight = new Vector2(transform.position.x + 0.3f, transform.position.y);
         hitLeft = Physics2D.Raycast(rayPositionLeft, Vector2.down, rayCastDist, groundMask);
         hitRight = Physics2D.Raycast(rayPositionRight, Vector2.down, rayCastDist, groundMask);
 
@@ -196,17 +203,26 @@ public class PlayerController : MonoBehaviour
         {
             playerState = 1;
             playerSR.color = transparentLevel[1];
+
+            source.clip = soundFX[0];
+            source.Play();
         }
         if (collision.gameObject.tag == "HellDeath" && playerState == 0)
         {
             playerState = 2;
             playerSR.color = transparentLevel[1];
+
+            source.clip = soundFX[1];
+            source.Play();
         }
 
         if (collision.gameObject.name == "Resurrector")
         {
             playerState = 0;
             playerSR.color = transparentLevel[0];
+
+            source.clip = soundFX[2];
+            source.Play();
         }
 
         if (collision.gameObject.tag == "Hazard" && playerState == 0)
